4e6af17c5295f99e08deaed6619d942d908ad3c5
THXBurke <rachelwheels@gmail.com>
2016-06-17 20:52:23 -0700

    form and data working!

--- app/js/entry.js
+++ app/js/entry.js
@@ -3,13 +3,27 @@ const angular = require('angular');
 var gitHubLU = angular.module('gitHubLU', []);
 
 gitHubLU.controller('UserController', ['$http', function($http) {
-  this.userName = [];
+  this.userName = '';
+  var stats = this;
   this.lookUp = function() {
     console.log('testing');
-    $http.get('https://api.github.com/user/' + this.userName + '/?access_token=' + process.env.GITHUB_TOKEN)
+    $http.get('https://api.github.com/users/' + this.userName /* '/?access_token=' +process.env.GITHUB_TOKEN*/)
     .then(
       function(res) {
-        console.log(res);
+        stats.data = {
+          name: res.data.name,
+          image: res.data.avatar_url,
+          email: res.data.email,
+          followers: res.data.followers,
+          following: res.data.following,
+          location: res.data.location,
+          repos: res.data.public_repos
+        };
+        stats.error = null;
+      },
+      function(err) {
+        console.log(err);
+        stats.error = 'not a valid GitHub user name';
       }
     );
   };
